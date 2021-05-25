d0ecbba7ccdd14af3588c44d66cae69d156974d5
Harrison Wilde <h.wilde@warwick.ac.uk>
2021-05-25 19:45:24 +0100

    Add bundle_samples call with internal_sampler to facilitate connects in other packages

--- src/MCMCTempering.jl
+++ src/MCMCTempering.jl
@@ -13,4 +13,15 @@ include("plotting.jl")
 
 export Tempered, TemperedSampler, plot_swaps, make_tempered_model, get_densities_and_θs, make_tempered_logπ, get_θ
 
+function AbstractMCMC.bundle_samples(
+    ts::Vector,
+    model::AbstractMCMC.AbstractModel,
+    sampler::TemperedSampler,
+    state::TemperedState,
+    chain_type::Type;
+    kwargs...
+)
+    AbstractMCMC.bundle_samples(ts, model, sampler.internal_sampler, state, chain_type; kwargs...)
+end
+
 end
