90722c9db33b8accc2a751fcef03f9d559c12945
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-10-20 01:10:04 +0100

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,8 @@ include("compat.jl")
     @testset "MvNormal 2D" begin
         d = 2
         nsamples = 20_000
+        swap_every_n = 2
+
         function logdensity(x)
             logpdf(MvNormal(ones(length(x)), I), x)
         end
@@ -26,35 +28,55 @@ include("compat.jl")
 
         # Set up our sampler with a joint multivariate Normal proposal.
         spl_inner = RWMH(MvNormal(zeros(d), 1e-1I))
-        spl = tempered(spl_inner, Δ, MCMCTempering.StandardSwap(); adapt=false, N_swap=2)
 
-        # Useful for analysis.
-        states = []
-        callback = StateHistoryCallback(states)
+        swapstrategies = [
+            MCMCTempering.StandardSwap(),
+            MCMCTempering.RandomPermutationSwap(),
+            MCMCTempering.NonReversibleSwap()
+        ]
+        @testset "$swapstrategy" for swapstrategy in swapstrategies
+            swapstrategy = MCMCTempering.NonReversibleSwap()
+            spl = tempered(spl_inner, Δ, swapstrategy; adapt=false, N_swap=swap_every_n)
 
-        # Sample.
-        samples = AbstractMCMC.sample(model, spl, nsamples; callback=callback);
-        states
+            # TODO: Remove or make use of.
+            # # Useful for analysis.
+            # states = []
+            # callback = StateHistoryCallback(states)
+            callback = (args...; kwargs...) -> nothing
 
-        # Extract the history of chain indices.
-        Δ_index_history_list = map(states) do state
-            state.Δ_index
+            # Sample.
+            samples = AbstractMCMC.sample(model, spl, nsamples; callback=callback, progress=false);
+
+            # # Extract the history of chain indices.
+            # Δ_index_history_list = map(states) do state
+            #     state.Δ_index
+            # end
+            # Δ_index_history = permutedims(reduce(hcat, Δ_index_history_list), (2, 1))
+
+            # Get example state.
+            state = states[end]
+            chain = if spl isa MCMCTempering.TemperedSampler
+                AbstractMCMC.bundle_samples(
+                    samples, model, spl.internal_sampler, MCMCTempering.state_for_chain(state), MCMCChains.Chains
+                )
+            else
+                AbstractMCMC.bundle_samples(samples, model, spl, state, MCMCChains.Chains)
+            end;
+
+            # Thin chain and discard burnin.
+            chain_thinned = chain[length(chain) ÷ 2 + 1:swap_every_n:end]
+            # Extract some summary statistics to compare.
+            desc = describe(chain_thinned)[1].nt
+            μ = desc.mean
+            σ = desc.std
+
+            # HACK: These bounds are quite generous. We're swapping quite frequently here
+            # so some of the strategies results in a rather large variance of the estimators
+            # it seems.
+            @test norm(μ - ones(length(μ))) ≤ 2e-1
+            @test norm(σ - ones(length(σ))) ≤ 3e-1
+
+            # TODO: Add some tests so ensure that we are doing _some_ swapping?
         end
-        Δ_index_history = permutedims(reduce(hcat, Δ_index_history_list), (2, 1))
-
-        # Get example state.
-        state = states[end]
-        chain = if spl isa MCMCTempering.TemperedSampler
-            AbstractMCMC.bundle_samples(
-                samples, model, spl.internal_sampler, state.states[first(state.chain_index)][2], MCMCChains.Chains
-            )
-        else
-            AbstractMCMC.bundle_samples(samples, model, spl, state, MCMCChains.Chains)
-        end;
-
-        
-        μ = mean(chain[length(chain) ÷ 2 + 1:10:end]).nt.mean
-        # HACK: This is quite a large threshold.
-        @test norm(μ - ones(length(μ))) ≤ 2e-1
     end
 end
