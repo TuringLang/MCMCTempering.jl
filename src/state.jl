d7b8096c05a08a0e3405451518d818e3e3228511
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-03-06 09:45:56 +0000

    reorederd stuff so it actually works

--- src/state.jl
+++ src/state.jl
@@ -157,6 +157,7 @@ state_for_chain(state::SwapState, I...) = state_for_process(state, chain_to_proc
 Return the state corresponding to the process indexed by `I...`.
 """
 state_for_process(state::SwapState, I...) = state_for_process(state.states, I...)
+state_for_process(states::AbstractArray, I...) = states[I...]
 
 """
     model_for_chain([ordering, ]sampler, model, state, I...)
@@ -171,60 +172,3 @@ model_for_chain(sampler, model, state, I...) = model_for_chain(ordering(sampler)
 Return the model corresponding to the process indexed by `I...`.
 """
 model_for_process(sampler, model, state, I...) = model_for_process(ordering(sampler), sampler, model, state, I...)
-
-
-"""
-    TemperedState
-
-A state for a tempered sampler.
-
-# Fields
-$(FIELDS)
-"""
-@concrete struct TemperedState
-    "state for swap-sampler"
-    swapstate
-    "state for the main sampler"
-    state
-    "inverse temperature for each of the chains"
-    chain_to_beta
-end
-
-# Defer extracting the corresponding state to the `swapstate`.
-state_for_process(state::TemperedState, I...) = state_for_process(state.swapstate, I...)
-
-# Here we make the model(s) using the temperatures.
-function model_for_process(sampler::TemperedSampler, model, state::TemperedState, I...)
-    return make_tempered_model(sampler, model, beta_for_process(state, I...))
-end
-
-function sampler_for_process(sampler::TemperedSampler, state::TemperedState, I...)
-    return _sampler_for_process_temper(sampler.sampler, state.swapstate, I...)
-end
-
-# If `sampler` is a `MultiSampler`, we assume it's ordered according to chains.
-_sampler_for_process_temper(sampler::MultiSampler, state, I...) = sampler.samplers[process_to_chain(state, I...)]
-# Otherwise, we just use the same sampler for everything.
-_sampler_for_process_temper(sampler, state, I...) = sampler
-
-"""
-    beta_for_chain(state[, I...])
-
-Return the β corresponding to the chain indexed by `I...`.
-If `I...` is not specified, the β corresponding to `β=1.0` will be returned.
-"""
-beta_for_chain(state::TemperedState) = beta_for_chain(state, 1)
-beta_for_chain(state::TemperedState, I...) = beta_for_chain(state.chain_to_beta, I...)
-# NOTE: Array impl. is useful for testing.
-beta_for_chain(chain_to_beta::AbstractArray, I...) = chain_to_beta[I...] 
-
-"""
-    beta_for_process(state, I...)
-
-Return the β corresponding to the process indexed by `I...`.
-"""
-beta_for_process(state::TemperedState, I...) = beta_for_process(state.chain_to_beta, state.swapstate.process_to_chain, I...)
-# NOTE: Array impl. is useful for testing.
-function beta_for_process(chain_to_beta::AbstractArray, proc2chain::AbstractArray, I...)
-    return beta_for_chain(chain_to_beta, process_to_chain(proc2chain, I...))
-end
