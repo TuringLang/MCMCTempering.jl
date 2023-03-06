53b7df8babe6b854198844eb3ce431c737a511bf
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-03-06 12:41:19 +0000

    fixed breaking bug in setparams_and_logprob!! for SwapState

--- test/compat.jl
+++ test/compat.jl
@@ -14,3 +14,12 @@ end
 
 # AdvancedHMC.jl
 MCMCTempering.getparams_and_logprob(t::AdvancedHMC.Transition) = t.z.θ, t.z.ℓπ.value
+MCMCTempering.getparams_and_logprob(state::AdvancedHMC.HMCState) = MCMCTempering.getparams_and_logprob(state.transition)
+
+# TODO: Implement `state_from` instead, to avoid re-computation of gradients if possible.
+function MCMCTempering.setparams_and_logprob!!(state::AdvancedHMC.HMCState, params, lp)
+    transition = state.transition
+    Setfield.@set! transition.z.θ = params
+    Setfield.@set! transition.z.ℓπ.value = lp
+    return Setfield.@set state.transition = transition
+end
