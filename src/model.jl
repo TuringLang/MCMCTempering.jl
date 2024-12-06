8b5345a2ad15e08ac587e47bc4aa342ae22b6ef8
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2024-12-06 08:36:26 +0000

    introduced `make_tempered_model_from_strategy` to avoid method ambiguities + `sample_and_logdensity` implemented for distribtuions

--- src/model.jl
+++ src/model.jl
@@ -15,41 +15,46 @@ A strategy which tempers the model to a reference model.
 # Fields
 $(FIELDS)
 """
-struct PathTemperingStrategy{D<:DistributionLogDensityProblem} <: AbstractTemperingStrategy
+struct PathTemperingStrategy{D} <: AbstractTemperingStrategy
     "reference model"
     reference::D
 end
-PathTemperingStrategy(dist::Distributions.Distribution) = PathTemperingStrategy(DistributionLogDensityProblem(dist))
 
 """
-    make_tempered_model([sampler, ]model, beta)
+    make_tempered_model(sampler, model, beta)
+    make_tempered_model(strategy, model, beta)
 
 Return an instance representing a `model` tempered with `beta`.
 
 The return-type depends on its usage in [`compute_logdensities`](@ref).
 """
-make_tempered_model(sampler, model, beta) = make_tempered_model(PowerTemperingStrategy(), model, beta)
-function make_tempered_model(::PowerTemperingStrategy, model, beta)
+make_tempered_model(strategy::AbstractTemperingStrategy, model, beta) = make_tempered_model_from_strategy(strategy, model, beta)
+# A wrapper for `LogDensityModel` to preserve the structure.
+# HACK: Need to find a better way to do this.
+function make_tempered_model(strategy::AbstractTemperingStrategy, model::AbstractMCMC.LogDensityModel, beta)
+    return AbstractMCMC.LogDensityModel(make_tempered_model(strategy, model.logdensity, beta))
+end
+
+"""
+    make_tempered_model_from_strategy(strategy::AbstractTemperingStrategy, model, beta)
+
+Return an instance representing a `model` tempered with `beta` using `strategy`.
+
+This method should be overloaded for both custom strategies and models.
+"""
+function make_tempered_model_from_strategy(::PowerTemperingStrategy, model, beta)
     if !implements_logdensity(model)
         error("`make_tempered_model` is not implemented for $(typeof(model)); either implement explicitly, or implement the LogDensityProblems.jl interface for `model`")
     end
 
     return TemperedLogDensityProblem(model, beta)
 end
-function make_tempered_model(::PathTemperingStrategy, model, beta)
+function make_tempered_model_from_strategy(strategy::PathTemperingStrategy, model, beta)
     if !implements_logdensity(model)
         error("`make_tempered_model` is not implemented for $(typeof(model)); either implement explicitly, or implement the LogDensityProblems.jl interface for `model`")
     end
 
-    return PathTemperedLogDensityProblem(model, beta)
-end
-# A wrapper for `LogDensityModel` to preserve the structure.
-# HACK: Need to find a better way to do this.
-function make_tempered_model(temperstrategy::PowerTemperingStrategy, model::AbstractMCMC.LogDensityModel, beta)
-    return AbstractMCMC.LogDensityModel(make_tempered_model(temperstrategy, model.logdensity, beta))
-end
-function make_tempered_model(temperstrategy::PathTemperingStrategy, model::AbstractMCMC.LogDensityModel, beta)
-    return AbstractMCMC.LogDensityModel(make_tempered_model(temperstrategy, model.logdensity, beta))
+    return PathTemperedLogDensityProblem(model, strategy.reference, beta)
 end
 
 
@@ -65,3 +70,13 @@ function logdensity(model, x)
     return LogDensityProblems.logdensity(model, x)
 end
 logdensity(model::AbstractMCMC.LogDensityModel, x) = LogDensityProblems.logdensity(model.logdensity, x)
+
+"""
+    sample_and_logprob(rng, model)
+
+Return a sample and its log-density from `model`.
+"""
+function sample_and_logdensity(rng::Random.AbstractRNG, dist::Distributions.Distribution)
+    params = rand(rng, dist)
+    return (params, logpdf(dist, params))
+end
--- src/samplers/iid.jl
+++ src/samplers/iid.jl
@@ -10,13 +10,8 @@ end
 getparams_and_logprob(s::IIDState) = (s.params, s.logprob)
 setparams_and_logprob!!(state::IIDState, params, logprob) = IIDState(params, logprob)
 
-function sample_and_logprob(rng::Random.AbstractRNG, sampler::IIDSampler{<:Distributions.Distribution})
-    params = rand(rng, sampler.dist)
-    return IIDState(params, logpdf(sampler.dist, params))
-end
-
 function AbstractMCMC.step(rng::Random.AbstractRNG, model::AbstractMCMC.LogDensityModel, sampler::IIDSampler, state=nothing; kwargs...)
-    params, logprob = sample_and_logprob(rng, sampler)
+    params, logprob = sample_and_logdensity(rng, sampler.dist)
     state = IIDState(params, logprob)
     return state, state
 end
